e52a128580974e0c13dc96554847a8225bb755c4
Julia <yyignateva@MacBook-Air-Yulia.local>
2024-05-20 13:03:19 +0300

    Added visual for all cards

--- Fool/ViewController.swift
+++ Fool/ViewController.swift
@@ -268,7 +268,7 @@ enum Denomination: Int {
 
 protocol Card {
     var suit: Suit { get }
-    var isTrump: Bool { get }
+    var isTrump: Bool { get set }
     var denomination: Denomination { get }
     
     func canBeat(_: Card) -> Bool
@@ -298,99 +298,179 @@ extension Card {
 
 final class Six: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .six
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Seven: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .seven
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Eight: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .eight
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Nine: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .nine
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Ten: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .ten
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Jack: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .jack
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Queen: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .queen
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class King: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .king
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
 }
 
 final class Ace: Card {
     let suit: Suit
-    let isTrump: Bool
+    var isTrump: Bool = false
     let denomination: Denomination = .ace
     
-     init(suit: Suit, isTrump: Bool) {
+     init(suit: Suit) {
          self.suit = suit
-         self.isTrump = isTrump
      }
+}
+
+struct VisualCard {
+    let image: UIImage
+    let heartsCards = "Cards/Hearts/"
+    let clubsCards = "Cards/Clubs/"
+    let diamondsCards = "Cards/Diamonds/"
+    let spadesCards = "Cards/Spades/"
+    
+    init(sixWithSuit: Suit) {
+        switch sixWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Six")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Six")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Six")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Six")!
+        }
+    }
+    
+    init(sevenWithSuit: Suit) {
+        switch sevenWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Seven")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Seven")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Seven")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Seven")!
+        }
+    }
+    
+    init(eightWithSuit: Suit) {
+        switch eightWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Eight")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Eight")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Eight")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Eight")!
+        }
+    }
+    
+    init(nineWithSuit: Suit) {
+        switch nineWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Nine")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Nine")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Nine")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Nine")!
+        }
+    }
+    
+    init(tenWithSuit: Suit) {
+        switch tenWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Ten")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Ten")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Ten")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Ten")!
+        }
+    }
+    
+    init(jackWithSuit: Suit) {
+        switch jackWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Jack")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Jack")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Jack")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Jack")!
+        }
+    }
+    
+    init(queenWithSuit: Suit) {
+        switch queenWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Queen")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Queen")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Queen")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Queen")!
+        }
+    }
+    
+    init(kingWithSuit: Suit) {
+        switch kingWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "King")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "King")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "King")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "King")!
+        }
+    }
+    
+    init(aceWithSuit: Suit) {
+        switch aceWithSuit {
+        case .clubs: image = UIImage(named: "\(clubsCards)" + "Ace")!
+        case .diamonds: image = UIImage(named: "\(diamondsCards)" + "Ace")!
+        case .hearts: image = UIImage(named: "\(heartsCards)" + "Ace")!
+        case .spades: image = UIImage(named: "\(spadesCards)" + "Ace")!
+        }
+    }
 }
